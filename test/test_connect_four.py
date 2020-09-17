c27e3e3032fd1843986f88afccc67a3b85ccb3b4
David E. Weirich <weirich.david@gmail.com>
2020-09-17 15:18:47 -0400

    Wrote soem tests

--- test/test_connect_four.py
+++ test/test_connect_four.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from connect_four.env import ConnectFourGame
 
@@ -42,3 +43,66 @@ def test_step_two_turns():
 
     assert np.array_equal(under_test.board, expected)
     assert under_test.turn == "X"
+
+
+def test_step_turns_in_same_col():
+    under_test = ConnectFourGame()
+
+    expected = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+
+    under_test.step(0)
+    under_test.step(0)
+
+    assert np.array_equal(under_test.board, expected)
+    assert under_test.turn == "X"
+
+
+def test_error_if_col_full():
+    under_test = ConnectFourGame()
+
+    under_test.board = np.array(
+        [
+            [2, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+
+    with pytest.raises(ValueError):
+        under_test.step(0)
+
+
+def test_reset():
+    under_test = ConnectFourGame()
+
+    expected = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+
+    under_test.step(1)
+    under_test.step(2)
+    under_test.step(1)
+    under_test.step(4)
+
+    under_test.reset()
+
+    assert np.array_equal(under_test.board, expected)
