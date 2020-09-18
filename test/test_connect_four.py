52b5665808e550a0e2ca5c105eda810b66500644
David E. Weirich <weirich.david@gmail.com>
2020-09-17 22:12:46 -0400

    tests for check_four_in_a_row

--- test/test_connect_four.py
+++ test/test_connect_four.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 
 from connect_four.env import ConnectFourGame
+from connect_four.env.connect_four_env import check_four_in_a_row
 
 
 def test_step():
@@ -98,6 +99,7 @@ def test_reset():
         ]
     )
 
+    # Do some stuff
     under_test.step(1)
     under_test.step(2)
     under_test.step(1)
@@ -106,3 +108,117 @@ def test_reset():
     under_test.reset()
 
     assert np.array_equal(under_test.board, expected)
+
+
+def test_check_four_in_a_row_empty_board():
+    board = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+
+    expected = 0
+
+    actual = check_four_in_a_row(board)
+
+    assert actual == expected
+
+
+def test_check_four_in_a_row_horizontal_winner():
+    board = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 2, 2, 2],
+        ]
+    )
+
+    expected = 1
+
+    actual = check_four_in_a_row(board)
+
+    assert actual == expected
+
+
+def test_check_four_in_a_row_vertical_winner():
+    board = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0],
+            [1, 2, 0, 0, 0, 0, 0],
+            [1, 2, 0, 0, 0, 0, 0],
+            [1, 2, 0, 0, 0, 0, 0],
+        ]
+    )
+
+    expected = 1
+
+    actual = check_four_in_a_row(board)
+
+    assert actual == expected
+
+
+def test_check_four_in_a_row_diagonal_winner():
+    board = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 2, 1, 0, 0, 0, 0],
+            [0, 2, 2, 1, 0, 0, 0],
+            [0, 2, 1, 2, 1, 0, 0],
+        ]
+    )
+
+    expected = 1
+
+    actual = check_four_in_a_row(board)
+
+    assert actual == expected
+
+
+def test_check_four_in_a_row_off_diagonal_winner():
+    board = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 2, 0, 0],
+            [0, 0, 1, 2, 2, 0, 0],
+            [0, 1, 2, 2, 1, 0, 0],
+        ]
+    )
+
+    expected = 1
+
+    actual = check_four_in_a_row(board)
+
+    assert actual == expected
+
+
+def test_check_four_in_a_row_player_two_wins():
+    board = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0],
+            [0, 2, 1, 0, 0, 0, 0],
+            [0, 2, 1, 1, 1, 0, 0],
+        ]
+    )
+
+    expected = 2
+
+    actual = check_four_in_a_row(board)
+
+    assert actual == expected
